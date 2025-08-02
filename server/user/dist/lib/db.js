ae520884cd6e02339d384eb0218b7a4451a65e05
aman <aman@sheelinfotech.in>
2025-08-02 23:23:07 +0530

    bug fix

---
+++ server/user/dist/lib/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import doten from "dotenv";
+doten.config();
+const connectToDb = async () => {
+    const url = process.env.MONGO_URL;
+    if (!url) {
+        throw new Error("MONGO_URL is not defined in the environment variables");
+    }
+    try {
+        await mongoose.connect(url, {
+            dbName: "LetsTalk",
+        });
+        console.log("Connected to MongoDB successfully");
+    }
+    catch (error) {
+        console.log("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+};
+export default connectToDb;
