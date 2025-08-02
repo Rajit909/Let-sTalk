bf5ed95824183d5d3d35b703deee53e43cae8406
ItsAnjaliSharma <anjalisharma243001@gmail.com>
2025-08-02 23:11:31 +0530

    add

--- server/user/src/config/db.ts
+++ server/user/src/config/db.ts
@@ -16,7 +16,6 @@ const connectDb=async()=>{
 });
         console.log("Database connected successfully");
     } catch (error) {
-        console.error("Database connection failed:", error);
         process.exit(1); // Exit the process with failure
     }
 }
